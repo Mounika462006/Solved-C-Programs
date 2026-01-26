2b3e69a7c95bb661982787aa01229c9423765ee8
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-26 18:41:29 +0530

    adding files

---
+++ ifelse2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a);
+    if(a%2==0){
+        printf("Even number");
+    }else{
+        printf("Odd number");
+    }
+    return 0;
+    
+}
