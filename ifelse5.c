8dfd89de37c72d108b117db39098764c2e88847e
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-26 19:04:08 +0530

    adding files

---
+++ ifelse5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a);
+
+    if((a % 4 ==0 ||a % 400 == 0) &&(a % 100 != 0) ){
+        printf("%d is a Leap Year",a);
+    }else{
+        printf("%d is not a Leap Year",a);
+    }
+    return 0;
+    
+}
