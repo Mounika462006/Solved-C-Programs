697d256fe4014281c2718d890458c6c860c989dc
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-27 12:22:01 +0530

    adding files

---
+++ ifelse25.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    printf("Enter option : \n");
+    int val;
+    scanf("%d",&val);
+    int a,b;
+
+    scanf("%d%d",&a,&b);
+    
+    switch (val)
+    {
+    case 1:
+        printf("Result = %d",a+b);
+        break;
+    case 2:
+        printf("Result = %d",a-b);
+        break;
+    case 3:
+        printf("Result = %d",a*b);
+        break;
+    case 4:
+        printf("Result = %d",a/b);
+        break;
+    
+    default:
+        printf("invalid input");
+        break;
+    }
+}
