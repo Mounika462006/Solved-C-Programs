f81f8211ef792fbbc10d4baf54cccfa548af60a6
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-30 05:09:11 +0530

    Add files via upload

--- For_problems/for22.c
+++ For_problems/for22.c
@@ -1,4 +1,21 @@
 #include<stdio.h>
 int main(){
+    int n;
+    scanf("%d",&n);
+
+    int count=0;
+    for(int i=2; i<=n;i++){
+        int isprime =1;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+            isprime =0;
+            }
+        }
+        if(isprime==1){
+            count++;
+        }
+            
+    }
+    printf("%d",count);
     return 0;
 }
