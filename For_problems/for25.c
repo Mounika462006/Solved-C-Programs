c8b6582667201de75525df1633db713682a3c91b
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-29 21:42:08 +0530

    Add files via upload

--- For_problems/for25.c
+++ For_problems/for25.c
@@ -1,4 +1,19 @@
 #include<stdio.h>
 int main(){
+    int n,cube;
+    scanf("%d",&n);
+    int sum=0;
+    for (int i=n; i!=0; i/=10){
+        int temp=i%10;
+        cube=temp*temp*temp;
+        sum=sum+cube;
+    }
+
+    if(sum==n){
+        printf("Yes");
+    }else{
+        printf("No");
+    }
+    
     return 0;
 }
