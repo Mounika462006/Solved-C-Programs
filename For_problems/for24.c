37429718503dfd522e2cff0c6e90639790d9e29f
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-29 21:50:34 +0530

    Add files via upload

--- For_problems/for24.c
+++ For_problems/for24.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
 int main(){
+    int n;
+    scanf("%d",&n);
+    int rev=0;
+    for(int i=n;i!=0;i/=10){
+        int temp=i%10;
+        rev=(rev*10)+temp;
+    }
+    if(rev==n){
+        printf("Yes");
+    }else{
+        printf("No");
+    }
     return 0;
 }
