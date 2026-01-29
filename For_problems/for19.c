0b664b8c935a7fad8a79c52090eec27adf5d1e68
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-29 21:20:05 +0530

    Add files via upload

--- For_problems/for19.c
+++ For_problems/for19.c
@@ -1,4 +1,15 @@
 #include<stdio.h>
 int main(){
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+        for(int i=1; i<=a*b; i++){
+            if(i % a==0 && i%b==0){
+                c=i;
+                break;
+        }
+    }
+    printf("%d",c);
     return 0;
 }
+
+
