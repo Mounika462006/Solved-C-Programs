ad73573cb89e9b36419edee01263c932cd589f65
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-29 16:01:38 +0530

    Add files via upload

---
+++ Nested_Loops/pattern13.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int k=0;
+    for(int i=0; i<=5; i++){
+        for(int j=1; j<=i;j++){
+            k=k+1;
+             printf("%d ",k);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
