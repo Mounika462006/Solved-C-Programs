d0ff4a0a4feb5b27fb7ce8c97a0619c7db9531ed
Mounika462006 <mounikanagarajan04@gmail.com>
2026-01-30 05:10:30 +0530

    Add files via upload

--- For_problems/for21.c
+++ For_problems/for21.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     int n;
-    scanf("%d",n);
-    for(int i=)
+    scanf("%d",&n);
+    int count=0;
+     int a=0;
+    for(int i=n; i!=0;i/=10){
+        int temp=i%10;
+        a =a+temp*pow(2,count);
+        count++;
+    }
+    printf("%d",a);
+    return 0;
 }
