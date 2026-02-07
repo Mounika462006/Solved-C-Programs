7f2ded54487c80bb231096179a36143783717d7b
Mounika462006 <mounikanagarajan04@gmail.com>
2026-02-07 13:52:57 +0530

    Add files via upload

---
+++ 2D_ArrayProblems/2d_array3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int row;
+    printf("Enter the row:");
+    scanf("%d",&row);
+    int column;
+    printf("Enter the column:");
+    scanf("%d",&column);
+    int arr[row][column];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int count=0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            if(arr[i][j]==0){
+                count++;
+            }
+        }
+    }
+    printf("Empty Seats = %d",count);
+
+    return 0;
+}
