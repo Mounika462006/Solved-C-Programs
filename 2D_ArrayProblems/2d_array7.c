95ba1b78839c665a305b8400309775b480a5332c
Mounika462006 <mounikanagarajan04@gmail.com>
2026-02-08 13:23:31 +0530

    Add files via upload

---
+++ 2D_ArrayProblems/2d_array7.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main(){
+    int row;
+    printf("Enter the row: ");
+    scanf("%d",&row);
+    int column;
+    printf("Enter the column: ");
+    scanf("%d",&column);
+    int arr[row][column];
+    int i,j;
+    for(i=0;i<row;i++){
+        for(j=0;j<column;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    // int max1=arr[i][j];//45
+    // int max2=-1;
+    // for(int i=0;i<row;i++){
+    //     for(int j=1;j<column;j++){
+    //         if(max1<arr[i][j]){
+    //             max2=max1;
+    //             max1=arr[i][j];
+                
+    //         }
+    //         else if(max2 >arr[i][j] && max1!=arr[i][j]){
+    //             max2=arr[i][j];
+    //         }
+    //     }
+    //     printf("Second : %d",max2);
+    // }
+    
+
+    int first =arr[0][0];
+    for(int i=1;i<row;i++){
+        for(int j=0; j<column;j++){
+            if(arr[i][j]>first){
+                first=arr[i][j];
+            }
+        }
+    }
+    int second=-1;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            if(second<arr[i][j] && arr[i][j]<first){
+                second=arr[i][j];
+            }
+        }
+    }
+    printf("2) %d",second);
+    return 0;
+}
