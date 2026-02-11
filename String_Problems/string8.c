227ebc4cd72c223c13bbc953e5722d7a30f38803
Mounika462006 <mounikanagarajan04@gmail.com>
2026-02-11 22:08:04 +0530

    Add files via upload

---
+++ String_Problems/string8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    printf("Enter the  string: ");
+    fgets(str,sizeof(str),stdin);
+    int count=0;
+    int i=0;
+    while(str[i]!='\0' && str[i] !='\n'){
+        if(str[i]==' '){
+            count++;
+        }
+
+        i++;
+    }
+    printf("%d",count);
+    return 0;
+}
