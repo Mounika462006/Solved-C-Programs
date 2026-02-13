e11406b54afa481edc83685ccb25bad2f1d620b9
Mounika462006 <mounikanagarajan04@gmail.com>
2026-02-13 11:41:43 +0530

    Add files via upload

---
+++ Functions_Problems/function3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void num(int a){
+    if(a % 2 ==0){
+        printf("Even");
+    }else{
+        printf("Odd");
+    }
+}
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    num(a);
+    return 0;
+}
