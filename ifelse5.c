00f80bd4cec667b8b6ef860d736fe55eec7a5cb9
Mounika462006 <mounikanagarajan04@gmail.com>
2026-02-13 13:32:57 +0530

    Fix leap year condition in ifelse5.c

--- ifelse5.c
+++ ifelse5.c
@@ -4,7 +4,7 @@ int main(){
     scanf("%d",&a);
     printf("%d\n",a);
 
-    if((a % 4 ==0 ||a % 400 == 0) &&(a % 100 != 0) ){
+    if((a % 4 ==0 && a % 400 == 0) || (a % 100 != 0) ){
         printf("%d is a Leap Year",a);
     }else{
         printf("%d is not a Leap Year",a);
