a931a7434ed4e1e788c71d4283957eb927577325
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-06-07 22:04:36 +0530

    Update pointer2.c

--- Pointers_Problem/pointer2.c
+++ Pointers_Problem/pointer2.c
@@ -13,7 +13,7 @@ int max(int *n,int *arr){
 int main(){
     int n;
     scanf("%d",&n);
-    int arr[n];
+    int arr[n];    
     int *p=arr;
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
