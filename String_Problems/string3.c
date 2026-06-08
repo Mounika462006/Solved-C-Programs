62874a78dcb41b83e30095b8b5cf4abf83b3447d
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-06-08 20:10:34 +0530

    Update string3.c

--- String_Problems/string3.c
+++ String_Problems/string3.c
@@ -3,7 +3,7 @@ int main(){
     char str[50];
     printf("Enter the string: ");
     fgets(str,sizeof(str),stdin);
-    int i=0;
+    int i=0;   
     while(str[i] !='\0' && str[i] !='@'){
         printf("%c",str[i]);
         i++;
