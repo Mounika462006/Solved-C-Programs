b5230c2ba4e7bdea93b368ec67520d101f941aa7
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-06-08 20:11:46 +0530

    Update string12.c

--- String_Problems/string12.c
+++ String_Problems/string12.c
@@ -9,7 +9,7 @@ int main(){
         if(!((str[i] >='a' && str[i] <='z') || (str[i]>='A'&& str[i]<='Z') || (str[i]>='0' && str[i]<='9')|| (str[i]==' '))){
             count++;
         }
-        i++;    
+        i++;        
     }
     printf("%d",count);
     return 0;
