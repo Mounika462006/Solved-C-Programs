0b98671f796c6f3e6260fef63cfcbbfe9495c7cc
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-06-08 20:09:33 +0530

    Update string14.c

--- String_Problems/string14.c
+++ String_Problems/string14.c
@@ -4,7 +4,7 @@ int main(){
     printf("Enter the  string: ");
     fgets(str1,sizeof(str1),stdin);
     char str2[100];
-    int i=0;
+    int i=0;    
     while (str1[i]!='\0'&& str1[i]!='\n')
     {
         str2[i]=str1[i];
