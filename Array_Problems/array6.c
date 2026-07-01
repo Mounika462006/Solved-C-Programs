b8ff035edd8fe4ae162a226a6393b83300c658eb
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-07-01 22:53:38 +0530

    Refactor array6.c for clarity and consistency

--- Array_Problems/array6.c
+++ Array_Problems/array6.c
@@ -6,8 +6,7 @@ int main() {
     int arr[n];
     for(int i=0;i < n;i++) {
         scanf("%d", &arr[i]);
-    }
-
+    }                
     int max = arr[0];
     for(int i=1;i<n;i++) {
         if (arr[i] > max) {
